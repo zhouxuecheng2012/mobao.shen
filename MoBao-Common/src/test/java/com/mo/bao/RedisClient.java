2d5ebc16155539bf04332cf7c0c9d71c758c8e79
zhouxuecheng2016 <zhouxuecheng2016@gmail>
2016-11-07 07:25:29 +0800

    wechat  token

---
+++ MoBao-Common/src/test/java/com/mo/bao/RedisClient.java
@@ -0,0 +1,7 @@
+package com.mo.bao;
+
+/**
+ * Created by hadoop on 2016/11/7.
+ */
+public class RedisClient {
+}
--- MoBao-Service/src/main/java/com/mo/bao/App.java
+++ MoBao-Service/src/main/java/com/mo/bao/App.java
@@ -1,13 +1,16 @@
 package com.mo.bao;
 
+
+import com.mo.bao.cache.CacheManager;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    private CacheManager cacheManager;
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
