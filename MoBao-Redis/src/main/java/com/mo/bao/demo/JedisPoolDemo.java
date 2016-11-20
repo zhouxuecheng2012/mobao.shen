ebb689b2bba49b12eba2a1d7bc85af1425ae22d8
zhouxuecheng2016 <zhouxuecheng2016@gmail>
2016-11-20 21:34:38 +0800

    file

---
+++ MoBao-Redis/src/main/java/com/mo/bao/demo/JedisPoolDemo.java
@@ -0,0 +1,14 @@
+package com.mo.bao.demo;
+
+/**
+ * Created by hadoop on 2016/11/20.
+ */
+public class JedisPoolDemo {
+
+    public static void main(String args[]) {
+
+
+
+    }
+
+}
---
+++ MoBao-Redis/src/main/java/com/mo/bao/demo/ShardedJedisDemo.java
@@ -0,0 +1,12 @@
+package com.mo.bao.demo;
+
+/**
+ * Created by hadoop on 2016/11/20.
+ */
+public class ShardedJedisDemo {
+
+    public static void main(String args[]) {
+
+    }
+
+}
